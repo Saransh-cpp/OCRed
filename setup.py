a6003c03eeacf1d3bab8e779c1dcb29cbeba48e9
Saransh <saransh0701@gmail.com>
2021-09-19 12:47:27 +0530

    Add files to create a package

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+f = open("requirements.txt", "r")
+install_requires = f.read().splitlines()
+f.close()
+
+classifiers = [
+  'Development Status :: 1 - Planning',
+  'Intended Audience :: Customer Service',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.9'
+]
+
+setup(
+    name="OCRModule",
+    version="0.1.0",
+    license="MIT",
+    description="The OCR module for AiView startup.",
+    packages=find_packages(),
+    classifiers=classifiers,
+    install_requires=install_requires
+)
