fcfb16e7d02e0283423c6d821f581377e25cb950
Saransh <saransh0701@gmail.com>
2022-07-25 21:49:19 +0530

    maintenance: switch to `pytest` and `pytest-cov`

--- tests/test_ocr.py
+++ tests/test_ocr.py
@@ -1,178 +1,175 @@
 import os
-import unittest
 
 from ocred.ocr import OCR
 
-
-class TestOCR(unittest.TestCase):
-    def setUp(self):
-        self.path_scanned = "images/Page.png"
-        self.path_real = "images/CosmosOne.jpg"
-        self.path_sign_board = "images/signboard.jpg"
-        self.path_invoice = "images/1146-receipt.jpg"
-
-    def test_ocr_with_scanned_image(self):
-
-        ocr = OCR(
-            False,
-            self.path_scanned,
-        )
-
-        self.assertEqual(ocr.path, self.path_scanned)
-        self.assertFalse(ocr.preprocess)
-
-        text = ocr.ocr_meaningful_text(save_output=True)
-
-        self.assertIsInstance(ocr.text, str)
-        self.assertIsInstance(text, str)
-        self.assertEqual(text, ocr.text)
-        assert os.path.exists("OCR.png")
-        assert os.path.exists("output.txt")
-        assert not os.path.exists("preprocessed.png")
-        assert not os.path.exists("audio.mp3")
-
-        ocr.text_to_speech()
-
-        assert os.path.exists("audio.mp3")
-
-        os.remove("audio.mp3")
-        os.remove("OCR.png")
-        os.remove("output.txt")
-
-    def test_ocr_with_real_image(self):
-
-        ocr = OCR(
-            True,
-            self.path_real,
-        )
-
-        self.assertEqual(ocr.path, "preprocessed.png")
-        self.assertTrue(ocr.preprocess)
-
-        text = ocr.ocr_meaningful_text()
-
-        self.assertIsInstance(ocr.text, str)
-        self.assertIsInstance(text, str)
-        self.assertEqual(text, ocr.text)
-        assert os.path.exists("OCR.png")
-        assert os.path.exists("preprocessed.png")
-        assert not os.path.exists("audio.mp3")
-
-        ocr.text_to_speech()
-
-        assert os.path.exists("audio.mp3")
-
-        os.remove("audio.mp3")
-        os.remove("OCR.png")
-        os.remove("preprocessed.png")
-
-    def test_ocr_sign_board(self):
-        ocr = OCR(
-            False,
-            self.path_sign_board,
-        )
-
-        self.assertEqual(ocr.path, self.path_sign_board)
-        self.assertFalse(ocr.preprocess)
-
-        text = ocr.ocr_sparse_text(save_output=True)
-
-        self.assertIsInstance(ocr.text, str)
-        self.assertIsInstance(text, str)
-        self.assertEqual(text, ocr.text)
-        assert os.path.exists("OCR.png")
-        assert os.path.exists("output.txt")
-        assert not os.path.exists("preprocessed.png")
-        assert not os.path.exists("audio.mp3")
-
-        ocr.text_to_speech(lang="hi")
-
-        assert os.path.exists("audio.mp3")
-
-        os.remove("audio.mp3")
-        os.remove("OCR.png")
-        os.remove("output.txt")
-
-    def test_ocr_invoices(self):
-        ocr = OCR(
-            False,
-            self.path_invoice,
-        )
-
-        self.assertEqual(ocr.path, self.path_invoice)
-        self.assertFalse(ocr.preprocess)
-
-        text = ocr.ocr_sparse_text()
-
-        self.assertIsInstance(ocr.text, str)
-        self.assertIsInstance(text, str)
-        self.assertEqual(text, ocr.text)
-        assert os.path.exists("OCR.png")
-        assert not os.path.exists("preprocessed.png")
-
-        extracted_info = ocr.process_extracted_text_from_invoice()
-
-        self.assertIsInstance(extracted_info, dict)
-        self.assertIsInstance(ocr.extracted_info, dict)
-        self.assertEqual(ocr.extracted_info, extracted_info)
-        self.assertTrue(
-            "price" in extracted_info
-            and "date" in extracted_info
-            and "place" in extracted_info
-            and "order_number" in extracted_info
-            and "phone_number" in extracted_info
-            and "post_processed_word_list" in extracted_info
-        )
-        self.assertIsInstance(extracted_info["price"], str)
-        self.assertIsInstance(extracted_info["date"], list)
-        self.assertTrue(len(extracted_info["date"]) == 0)
-        self.assertIsInstance(extracted_info["place"], str)
-        self.assertIsInstance(extracted_info["phone_number"], list)
-        self.assertTrue(len(extracted_info["phone_number"]) == 1)
-        self.assertIsInstance(extracted_info["order_number"], int)
-        self.assertIsInstance(extracted_info["post_processed_word_list"], list)
-
-        self.path_invoice = "images/1166-receipt.jpg"
-        ocr = OCR(
-            False,
-            self.path_invoice,
-        )
-
-        self.assertEqual(ocr.path, self.path_invoice)
-        self.assertFalse(ocr.preprocess)
-
-        text = ocr.ocr_sparse_text()
-
-        self.assertIsInstance(ocr.text, str)
-        self.assertIsInstance(text, str)
-        self.assertEqual(text, ocr.text)
-        assert os.path.exists("OCR.png")
-        assert not os.path.exists("preprocessed.png")
-
-        extracted_info = ocr.process_extracted_text_from_invoice()
-
-        self.assertIsInstance(extracted_info, dict)
-        self.assertIsInstance(ocr.extracted_info, dict)
-        self.assertEqual(ocr.extracted_info, extracted_info)
-        self.assertTrue(
-            "price" in extracted_info
-            and "date" in extracted_info
-            and "place" in extracted_info
-            and "order_number" in extracted_info
-            and "phone_number" in extracted_info
-            and "post_processed_word_list" in extracted_info
-        )
-        self.assertIsInstance(extracted_info["price"], str)
-        self.assertIsInstance(extracted_info["date"], list)
-        self.assertTrue(len(extracted_info["date"]) == 1)
-        self.assertIsInstance(extracted_info["place"], str)
-        self.assertIsInstance(extracted_info["phone_number"], list)
-        self.assertTrue(len(extracted_info["phone_number"]) == 0)
-        self.assertIsInstance(extracted_info["order_number"], str)
-        self.assertIsInstance(extracted_info["post_processed_word_list"], list)
-
-        os.remove("OCR.png")
+path_scanned = "images/Page.png"
+path_real = "images/CosmosOne.jpg"
+path_sign_board = "images/signboard.jpg"
+path_invoice = "images/1146-receipt.jpg"
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_ocr_with_scanned_image():
+
+    ocr = OCR(
+        False,
+        path_scanned,
+    )
+
+    assert ocr.path == path_scanned
+    assert ocr.preprocess is False
+
+    text = ocr.ocr_meaningful_text(save_output=True)
+
+    assert isinstance(ocr.text, str)
+    assert isinstance(text, str)
+    assert text == ocr.text
+    assert os.path.exists("OCR.png")
+    assert os.path.exists("output.txt")
+    assert not os.path.exists("preprocessed.png")
+    assert not os.path.exists("audio.mp3")
+
+    ocr.text_to_speech()
+
+    assert os.path.exists("audio.mp3")
+
+    os.remove("audio.mp3")
+    os.remove("OCR.png")
+    os.remove("output.txt")
+
+
+def test_ocr_with_real_image():
+
+    ocr = OCR(
+        True,
+        path_real,
+    )
+
+    assert ocr.path == "preprocessed.png"
+    assert ocr.preprocess is True
+
+    text = ocr.ocr_meaningful_text()
+
+    assert isinstance(ocr.text, str)
+    assert isinstance(text, str)
+    assert text == ocr.text
+    assert os.path.exists("OCR.png")
+    assert os.path.exists("preprocessed.png")
+    assert not os.path.exists("audio.mp3")
+
+    ocr.text_to_speech()
+
+    assert os.path.exists("audio.mp3")
+
+    os.remove("audio.mp3")
+    os.remove("OCR.png")
+    os.remove("preprocessed.png")
+
+
+def test_ocr_sign_board():
+    ocr = OCR(
+        False,
+        path_sign_board,
+    )
+
+    assert ocr.path == path_sign_board
+    assert ocr.preprocess is False
+
+    text = ocr.ocr_sparse_text(save_output=True)
+
+    assert isinstance(ocr.text, str)
+    assert isinstance(text, str)
+    assert text == ocr.text
+    assert os.path.exists("OCR.png")
+    assert os.path.exists("output.txt")
+    assert not os.path.exists("preprocessed.png")
+    assert not os.path.exists("audio.mp3")
+
+    ocr.text_to_speech(lang="hi")
+
+    assert os.path.exists("audio.mp3")
+
+    os.remove("audio.mp3")
+    os.remove("OCR.png")
+    os.remove("output.txt")
+
+
+def test_ocr_invoices():
+    global path_invoice
+    ocr = OCR(
+        False,
+        path_invoice,
+    )
+
+    assert ocr.path == path_invoice
+    assert ocr.preprocess is False
+
+    text = ocr.ocr_sparse_text()
+
+    assert isinstance(ocr.text, str)
+    assert isinstance(text, str)
+    assert text == ocr.text
+    assert os.path.exists("OCR.png")
+    assert not os.path.exists("preprocessed.png")
+
+    extracted_info = ocr.process_extracted_text_from_invoice()
+
+    assert isinstance(extracted_info, dict)
+    assert isinstance(ocr.extracted_info, dict)
+    assert ocr.extracted_info == extracted_info
+    assert (
+        "price" in extracted_info
+        and "date" in extracted_info
+        and "place" in extracted_info
+        and "order_number" in extracted_info
+        and "phone_number" in extracted_info
+        and "post_processed_word_list" in extracted_info
+    ) is True
+    assert isinstance(extracted_info["price"], str)
+    assert isinstance(extracted_info["date"], list)
+    assert len(extracted_info["date"]) == 0
+    assert isinstance(extracted_info["place"], str)
+    assert isinstance(extracted_info["phone_number"], list)
+    assert len(extracted_info["phone_number"]) == 1
+    assert isinstance(extracted_info["order_number"], int)
+    assert isinstance(extracted_info["post_processed_word_list"], list)
+
+    path_invoice = "images/1166-receipt.jpg"
+    ocr = OCR(
+        False,
+        path_invoice,
+    )
+
+    assert ocr.path == path_invoice
+    assert ocr.preprocess is False
+
+    text = ocr.ocr_sparse_text()
+
+    assert isinstance(ocr.text, str)
+    assert isinstance(text, str)
+    assert text == ocr.text
+    assert os.path.exists("OCR.png")
+    assert not os.path.exists("preprocessed.png")
+
+    extracted_info = ocr.process_extracted_text_from_invoice()
+
+    assert isinstance(extracted_info, dict)
+    assert isinstance(ocr.extracted_info, dict)
+    assert ocr.extracted_info == extracted_info
+    assert (
+        "price" in extracted_info
+        and "date" in extracted_info
+        and "place" in extracted_info
+        and "order_number" in extracted_info
+        and "phone_number" in extracted_info
+        and "post_processed_word_list" in extracted_info
+    ) is True
+    assert isinstance(extracted_info["price"], str)
+    assert isinstance(extracted_info["date"], list)
+    assert len(extracted_info["date"]) == 1
+    assert isinstance(extracted_info["place"], str)
+    assert isinstance(extracted_info["phone_number"], list)
+    assert len(extracted_info["phone_number"]) == 0
+    assert isinstance(extracted_info["order_number"], str)
+    assert isinstance(extracted_info["post_processed_word_list"], list)
+
+    os.remove("OCR.png")
--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,88 +1,84 @@
 import os
-import unittest
 
 import cv2
 import numpy as np
 
 from ocred.preprocessing import Preprocessor
 
-
-class TestPreprocessing(unittest.TestCase):
-    def setUp(self):
-        self.path = "images/CosmosOne.jpg"
-
-    def test_scan(self):
-        pre = Preprocessor(self.path)
-        self.assertEqual(pre.path, self.path)
-
-        img = pre.scan()
-        self.assertIsInstance(img, np.ndarray)
-
-        img = pre.scan(save=True)
-        self.assertIsInstance(img, np.ndarray)
-        assert os.path.exists("scanned.png")
-
-        os.remove("scanned.png")
-
-        img = cv2.imread(self.path)
-
-        img = pre.scan(image=img)
-        self.assertIsInstance(img, np.ndarray)
-
-    def test_rotate(self):
-        pre = Preprocessor(self.path)
-        self.assertEqual(pre.path, self.path)
-
-        img, median_angle = pre.rotate()
-        self.assertIsInstance(median_angle, float)
-        self.assertIsInstance(img, np.ndarray)
-
-        img, median_angle = pre.rotate(save=True)
-        self.assertIsInstance(img, np.ndarray)
-        self.assertIsInstance(median_angle, float)
-        assert os.path.exists("rotated.png")
-
-        os.remove("rotated.png")
-
-        img, median_angle = pre.rotate(image=img)
-        img1, median_angle = pre.rotate(image=img)
-        self.assertIsInstance(median_angle, float)
-        self.assertIsInstance(img, np.ndarray)
-        self.assertIsInstance(img1, np.ndarray)
-        np.testing.assert_array_equal(img, img1)
-
-    def test_remove_noise(self):
-        pre = Preprocessor(self.path)
-        self.assertEqual(pre.path, self.path)
-
-        img = pre.remove_noise()
-        self.assertIsInstance(img, np.ndarray)
-
-        img = pre.remove_noise(save=True)
-        self.assertIsInstance(img, np.ndarray)
-        assert os.path.exists("noise_free.png")
-
-        os.remove("noise_free.png")
-
-        img = pre.remove_noise(image=img)
-        self.assertIsInstance(img, np.ndarray)
-
-    def test_thicken_font(self):
-        pre = Preprocessor(self.path)
-        self.assertEqual(pre.path, self.path)
-
-        img = pre.thicken_font()
-        self.assertIsInstance(img, np.ndarray)
-
-        img = pre.thicken_font(save=True)
-        self.assertIsInstance(img, np.ndarray)
-        assert os.path.exists("thick_font.png")
-
-        os.remove("thick_font.png")
-
-        img = pre.thicken_font(image=img)
-        self.assertIsInstance(img, np.ndarray)
+path = "images/CosmosOne.jpg"
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_scan():
+    pre = Preprocessor(path)
+    assert pre.path == path
+
+    img = pre.scan()
+    assert isinstance(img, np.ndarray)
+
+    img = pre.scan(save=True)
+    assert isinstance(img, np.ndarray)
+    assert os.path.exists("scanned.png")
+
+    os.remove("scanned.png")
+
+    img = cv2.imread(path)
+
+    img = pre.scan(image=img)
+    assert isinstance(img, np.ndarray)
+
+
+def test_rotate():
+    pre = Preprocessor(path)
+    assert pre.path == path
+
+    img, median_angle = pre.rotate()
+    assert isinstance(median_angle, float)
+    assert isinstance(img, np.ndarray)
+
+    img, median_angle = pre.rotate(save=True)
+    assert isinstance(img, np.ndarray)
+    assert isinstance(median_angle, float)
+    assert os.path.exists("rotated.png")
+
+    os.remove("rotated.png")
+
+    img, median_angle = pre.rotate(image=img)
+    img1, median_angle = pre.rotate(image=img)
+    assert isinstance(median_angle, float)
+    assert isinstance(img, np.ndarray)
+    assert isinstance(img1, np.ndarray)
+    np.testing.assert_array_equal(img, img1)
+
+
+def test_remove_noise():
+    pre = Preprocessor(path)
+    assert pre.path == path
+
+    img = pre.remove_noise()
+    assert isinstance(img, np.ndarray)
+
+    img = pre.remove_noise(save=True)
+    assert isinstance(img, np.ndarray)
+    assert os.path.exists("noise_free.png")
+
+    os.remove("noise_free.png")
+
+    img = pre.remove_noise(image=img)
+    assert isinstance(img, np.ndarray)
+
+
+def test_thicken_font():
+    pre = Preprocessor(path)
+    assert pre.path == path
+
+    img = pre.thicken_font()
+    assert isinstance(img, np.ndarray)
+
+    img = pre.thicken_font(save=True)
+    assert isinstance(img, np.ndarray)
+    assert os.path.exists("thick_font.png")
+
+    os.remove("thick_font.png")
+
+    img = pre.thicken_font(image=img)
+    assert isinstance(img, np.ndarray)
